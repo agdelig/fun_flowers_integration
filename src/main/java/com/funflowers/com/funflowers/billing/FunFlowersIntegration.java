11d81aa455d944e819cbfc2497507b9d964a17b1
agdelig <agdelig1984@yahoo.gr>
2018-10-13 10:16:48 +0100

    Initial commit.

---
+++ src/main/java/com/funflowers/com/funflowers/billing/FunFlowersIntegration.java
@@ -0,0 +1,28 @@
+package com.funflowers.com.funflowers.billing;
+
+import com.flexionmobile.codingchallenge.integration.Integration;
+import com.flexionmobile.codingchallenge.integration.Purchase;
+import java.util.List;
+
+/**
+ *
+ * @author Agelos
+ */
+public class FunFlowersIntegration implements Integration{
+
+    @Override
+    public Purchase buy(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Purchase> getPurchases() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void consume(Purchase prchs) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/main/java/com/funflowers/com/funflowers/billing/FunFlowersPurchase.java
@@ -0,0 +1,26 @@
+package com.funflowers.com.funflowers.billing;
+
+import com.flexionmobile.codingchallenge.integration.Purchase;
+
+/**
+ *
+ * @author Agelos
+ */
+public class FunFlowersPurchase implements Purchase{
+
+    @Override
+    public String getId() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean getConsumed() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getItemId() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
